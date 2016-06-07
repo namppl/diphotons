e6677bafe4d0a88c4f1384f7c4c60df574e4bc1e
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2016-06-07 14:23:20 +0200

    first commit

---
+++ Analysis/macros/analyzeToys_all_ZG.sh
@@ -0,0 +1,6 @@
+./analyzeToys.sh toys_mcBIAS_v4_pow
+./analyzeToys.sh toys_mcBIAS_v4_expow2
+./analyzeToys.sh toys_mcBIAS_v4_sumexp3
+./analyzeToys.sh toys_mcBIAS_v4_moddijet
+./analyzeToys.sh toys_mcBIAS_v4_invpow
+./analyzeToys.sh toys_mcBIAS_v4_invpowlin
